069788debbd4bc249a7af46806b31e38857c5d00
Shivendra Hada <shivendrahada@192.168.1.3>
2025-05-24 12:54:32 +0530

    Updated title and description

--- src/layouts/Meta.tsx
+++ src/layouts/Meta.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import { NextSeo } from 'next-seo';
+import Head from "next/head";
+import { NextSeo } from "next-seo";
 
-import { AppConfig } from '@/utils/AppConfig';
+import { AppConfig } from "@/utils/AppConfig";
 
 type IMetaProps = {
   title: string;
@@ -13,6 +13,18 @@ const Meta = (props: IMetaProps) => (
   <>
     <Head>
       <meta charSet="UTF-8" key="charset" />
+      <title>
+        Reliable Taxi Booking Service from Bhopal to All India | Safe &
+        Affordable Rides
+      </title>
+      <meta
+        name="description"
+        content="Book your taxi from Bhopal to any city in India with ease. Trusted, safe, and affordable taxi service for outstation and local travel. 24/7 booking support and professional drivers."
+      />
+      <meta
+        name="keywords"
+        content="Taxi booking Bhopal, Outstation taxi Bhopal, Bhopal to Delhi taxi, Affordable taxi service India, Long distance taxi booking, Reliable taxi service Bhopal, India taxi booking online, Taxi service near me, Safe taxi booking India, Book taxi online India"
+      />
       <meta
         name="viewport"
         content="width=device-width,initial-scale=1"
